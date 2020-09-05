ba75f9b908d892941e3f4f488ebc881b410d4d4e
Rp Vrushank <rpvrushank39@gmail.com>
2020-09-05 19:30:45 +0530

    Emissions

---
+++ plot3.R
@@ -0,0 +1,13 @@
+## reading files
+emissions_data <- readRDS("summarySCC_PM25.rds")
+class_code <- readRDS("Source_Classification_Code.rds")
+
+## ggplot2 plotting system to make a plot 
+baltimore_data <- subset(emissions_data, emissions_data$fips=="24510")
+baltimore_type_year <- aggregate(baltimore_data$Emissions, by=list(baltimore_data$type, baltimore_data$year),  FUN=sum)
+colnames(baltimore_type_year) <- c("Type", "Year", "Emissions")
+
+library(ggplot2)
+png(filename = "plot3.png")
+qplot(Year, Emissions, data = baltimore_type_year, color = Type, geom = "line") +ggtitle("Total Emissions of PM2.5 in Baltimore City By pollutant type") + ylab("Total Emissions (tons)") + xlab("Year") 
+dev.off()
